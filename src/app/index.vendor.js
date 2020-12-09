14844e20e2619ffb3180b3d939370aa6e05a2880
Kaciaryna Dubouskaya <rinawwwster@gmail.com>
2020-12-09 15:59:43 +0300

    Initial commit

---
+++ src/app/index.vendor.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// node_modules
+import "angular";
+
+import "@uirouter/angularjs";
+
+
+import "angular-animate";
+
+import "angular-sanitize";
+
+
+
+// local scripts
+//import "../assets/js/...";
